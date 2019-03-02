07b68439da3059088417e7d24bfb120daf8d03ce
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 11:48:10 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import {Switch, Route} from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+
+import Home from "./containers/Home/Home";
+import ContactAdd from "./containers/ContactAdd/ContactAdd";
+import ContactEdit from "./containers/ContactEdit/ContactEdit";
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Layout>
+                    <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/contacts/add" exact component={ContactAdd}/>
+                        <Route path="/contacts/:id/edit" component={ContactEdit}/>
+                    </Switch>
+                </Layout>
+            </div>
+        );
+    }
 }
 
 export default App;
+
