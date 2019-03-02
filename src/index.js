e7277e34e6c1d666b32d2cf01b89a247a9ec8bb4
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 17:36:00 +0600

    add reducer

--- src/index.js
+++ src/index.js
@@ -4,18 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
-import {createStore, applyMiddleware, compose} from 'redux';
+import {createStore, applyMiddleware} from 'redux';
 import thunkMiddleware from 'redux-thunk';
 import {Provider} from 'react-redux';
+import reducer from './store/reducers/contactReducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(
-    composeEnhancers(applyMiddleware(thunkMiddleware))
+const store = createStore(reducer, applyMiddleware(thunkMiddleware)
 );
 
 const app = (
-    <Provider store = {store}>
+    <Provider store={store}>
         <BrowserRouter>
             <App/>
         </BrowserRouter>
