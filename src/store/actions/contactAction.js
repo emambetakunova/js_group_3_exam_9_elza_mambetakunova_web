c393542a0a5165b5d743957b428e109cd7449b32
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 11:38:29 +0600

    connect types with action

---
+++ src/store/actions/contactAction.js
@@ -0,0 +1,62 @@
+import {CONTACT_FAILURE, CONTACT_REQUEST, CONTACT_SUCCESS, CONTACT_EDIT_SUCCESS, CONTACT_EDITED} from "./actionsTypes";
+import axios from '../../axios-contact';
+
+export const contactRequest = () => ({type: CONTACT_REQUEST});
+
+export const contactSuccess = response => ({type: CONTACT_SUCCESS, response});
+
+export const contactFailure = error => ({type: CONTACT_FAILURE, error});
+
+export const contactEditSuccess = response => ({type: CONTACT_EDIT_SUCCESS, response});
+
+export const contactEdited = () => ({type: CONTACT_EDITED});
+
+export const fetchContacts = () => {
+    return dispatch => {
+        dispatch(contactRequest());
+        axios.get('/contacts.json').then(response => {
+            dispatch(contactSuccess(response.data));
+        }, error => {
+            dispatch(contactFailure());
+        });
+    }
+};
+
+export const deleteContact = (id) => {
+    return dispatch => {
+        dispatch(contactRequest());
+        axios.delete('/contacts/' + id + '.json').then(() => {
+            dispatch(fetchContacts());
+        }, error => {
+            dispatch(contactFailure());
+        });
+    }
+};
+
+export const addContact = (history, contact) => {
+    axios.post('contacts.json', contact).then(() => {
+            history.push('/')
+        }
+    );
+};
+
+export const goToEditContact = (id) => {
+    return dispatch => {
+        return axios.get('/contacts/' + id + '.json').then((response) => {
+                dispatch(contactEditSuccess(response.data));
+            },
+            error => dispatch(contactFailure(error))
+        );
+    }
+};
+
+export const editContact = (id, history, contact) => {
+    return dispatch => {
+        axios.put('/contacts/' + id + '.json', contact).then(() => {
+                dispatch(contactEdited());
+                history.push('/')
+            },
+            error => dispatch(contactFailure(error))
+        );
+    }
+};
