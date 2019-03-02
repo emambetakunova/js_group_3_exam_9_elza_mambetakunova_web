b59ee35845463eabd2be6045d5e4d4dde81b55ad
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 17:34:49 +0600

    add styles and refactor code

--- src/containers/ContactList/ContactList.js
+++ src/containers/ContactList/ContactList.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './ContactList.css';
 
-const ContactList = props => {
-    return (
-        <div className="ContactList">
-            <img className="ContactImage" src={props.image} alt="contact"/>
-            <p>{props.contactName}</p>
-            <button className="Button" onClick={props.goToEdit}>Edit</button>
-            <button className="Button" onClick={props.delete}>Delete</button>
-        </div>
-    );
+const ContactList = props =>  {
+        return (
+            <div className="ContactList">
+                <img className="ContactImage" src={props.image} alt="contact"/>
+                <p>{props.contactName}</p>
+                <button onClick={props.open}>Show more</button>
+            </div>
+        );
 };
 
 export default ContactList;
