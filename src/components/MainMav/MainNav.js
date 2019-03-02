ddbb887802d878967c15e2a143d1e55b553ea72b
Elza Mambetakunova <elza.mambetakunova@gmail.com>
2019-03-02 11:26:40 +0600

    add home, add pages

--- src/components/MainMav/MainNav.js
+++ src/components/MainMav/MainNav.js
@@ -9,10 +9,9 @@ class Nav extends Component {
     render() {
         return (
             <nav className="MainNav">
-                <h3 className="Logo">Pizza House</h3>
+                <h3 className="Logo">Contacts</h3>
                 <NavLink className="NavLink"   activeClassName="Active" exact={true} to="/">Home</NavLink>
-                <NavLink className="NavLink"  activeClassName="Active" to="/dishes/add">Add</NavLink>
-                <NavLink className="NavLink"  activeClassName="Active" to="/dishes/orders">Orders</NavLink>
+                <NavLink className="NavLink"  activeClassName="Active" to="/contacts/add">Add</NavLink>
             </nav>
         );
     }
